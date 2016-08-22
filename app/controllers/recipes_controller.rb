fc4e9dfe287b36daea36fcc819263de8bf5d0d1c
dowdje <dowdje89@gmail.com>
2016-08-22 19:44:04 -0400

    Andrew weenBear

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -9,8 +9,28 @@ class RecipesController < ApplicationController
 
   def new
     @recipe = Recipe.new
+    i = 1
+    10.times do
+    @recipe.ingredients.build
+
+  end
   end
 
   def create
+
+    @recipe = Recipe.create(recipe_params)
+    redirect_to recipe_path(@recipe)
+  end
+
+  private
+
+  def recipe_params
+    params[:recipe][:ingredients_attributes].delete_if {|key, value| value[:name] == ""}
+    params.require(:recipe).permit(
+      :title,
+      ingredients_attributes: [
+        :name,
+        :quantity]
+      )
   end
 end
